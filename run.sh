a7bdb0422e5b9f32269547ee26934e8ad35f6977
NorbertZheng <2508440340@qq.com>
2024-10-15 17:48:20 +0800

    Du-IN: init skeleton of run.sh, build pass

---
+++ run.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+## Du-IN
+cd ./train/duin
+# 001
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 001 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 001 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/001/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 001 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/001/mae/model/checkpoint-399.pth
+# 002
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 002 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 002 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/002/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 002 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/002/mae/model/checkpoint-399.pth
+# 003
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 003 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 003 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/003/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 003 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/003/mae/model/checkpoint-399.pth
+# 004
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 004 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 004 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/004/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 004 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/004/mae/model/checkpoint-399.pth
+# 005
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 005 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 005 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/005/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 005 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/005/mae/model/checkpoint-399.pth
+# 006
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 006 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 006 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/006/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 006 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/006/mae/model/checkpoint-399.pth
+# 007
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 007 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 007 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/007/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 007 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/007/mae/model/checkpoint-399.pth
+# 008
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 008 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 008 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/008/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 008 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/008/mae/model/checkpoint-399.pth
+# 009
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 009 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 009 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/009/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 009 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/009/mae/model/checkpoint-399.pth
+# 010
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 010 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 010 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/010/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 010 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/010/mae/model/checkpoint-399.pth
+# 011
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 011 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 011 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/011/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 011 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/011/mae/model/checkpoint-399.pth
+# 012
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 012 --subj_idxs 0
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 012 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/012/vqvae/model/checkpoint-399.pth
+python run_cls.py --seeds 42 0 1 2 3 4 --subjs 012 --subj_idxs 0\
+    --pt_ckpt ./pretrains/duin/012/mae/model/checkpoint-399.pth
+
